05557c19e5602bddb67fe535c4179640b4ecb31d
asdoll <ludeqqwo@gmail.com>
2024-10-13 22:58:27 +0800

    init commit

---
+++ android/app/src/main/kotlin/com/example/skana_pix/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.skana_pix
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/samples/illust_samples.dart
@@ -0,0 +1,12 @@
+import 'package:pixiv_dart_api/pixiv_dart_api.dart';
+
+Future<List<Illust>> getIllustSamples() async {
+  var account = await Account.fromPath();
+  var apiClient = ApiClient(account!, PDio());
+  var res = await apiClient.getUserIllusts("35082639", "manga");
+  if (res.success) {
+    return res.data;
+  } else {
+    return [];
+  }
+}
